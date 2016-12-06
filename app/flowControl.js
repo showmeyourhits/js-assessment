9afa2680646a32ce39418c1b6eb0d2f3fe058c83
tk_user <tk_user@dtulyakov.local>
2016-12-06 19:07:03 +0300

    flow control complete

--- app/flowControl.js
+++ app/flowControl.js
@@ -2,6 +2,14 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.flowControlAnswers = {
   fizzBuzz: function(num) {
+    switch(typeof num){
+        case "number":
+            return num % 3 === 0 ? (num % 5 === 0 ? "fizzbuzz" : "fizz") : num % 5 === 0 ? "buzz" : num; 
+            break;
+        default:
+            return false;
+            break;
+    }
     // write a function that receives a number as its argument;
     // if the number is divisible by 3, the function should return 'fizz';
     // if the number is divisible by 5, the function should return 'buzz';

8e678211ed7108c263f8a49981a4c00c88e4dc26
tk_user <tk_user@dtulyakov.local>
2016-12-06 17:41:59 +0300

    async completed

--- app/async.js
+++ app/async.js
@@ -2,10 +2,20 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-
+  	return new Promise(function(res, rej){
+  		setTimeout(function(){
+  			res(value);
+  		}, 0);
+  	});
   },
 
   manipulateRemoteData: function(url) {
-
+  	return fetch(url).then(function(response){
+  		return response.json().then(function(data){
+  			return data["people"].map((el)=>{
+	  			return el["name"];
+	  		}).sort();
+  		}); 		
+  	});
   }
 };

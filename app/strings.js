76b6e0f50762c394f86b5185d82d827b3dfc9df3
tk_user <tk_user@dtulyakov.local>
2016-12-07 15:22:12 +0300

    strings complete

--- app/strings.js
+++ app/strings.js
@@ -2,14 +2,29 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.stringsAnswers = {
   reduceString: function(str, amount) {
-
+  	return str.replace(/(\w)\1+/g, (match)=>{
+  		return match.slice(0, amount);
+  	});
   },
 
   wordWrap: function(str, cols) {
-
+  	let res = str.split(''),
+  		re = /\w/;
+  	for(let i = cols; i <= str.length; i += cols){
+  		if(re.test(str[i])){
+  			let space = str.slice(i - cols, i).lastIndexOf(" ");
+  			if(space !== -1){
+  				res[i - cols + space] = "\n";
+  			}
+  		}
+  		else{
+  			res[i] = "\n";
+  		}
+  	}
+  	return res.join('');
   },
 
   reverseString: function(str) {
-
+  	return str.split('').reverse().join('');
   }
 };
